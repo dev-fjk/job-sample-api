ccfe1083f52b6aaf13c8d22a675ef839857915eb
“daihukumaron” <maron.wankoo@icloud.com>
2022-12-04 09:35:02 +0900

    応募情報についてRepositoryのfindOneメソッドに説明を追加

--- src/main/java/com/gw/job/sample/repository/PostedCompanyRepository.java
+++ src/main/java/com/gw/job/sample/repository/PostedCompanyRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import com.gw.job.sample.dao.PostedCompanyDao;
 import com.gw.job.sample.entity.doma.PostedCompany;
+import com.gw.job.sample.entity.response.PostedResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,7 +20,12 @@ public class PostedCompanyRepository {
 	@Autowired
 	private final PostedCompanyDao postedCompanyDao;
 
-
+    /**
+     * 応募情報を取得する
+     * @param userId    ユーザーID
+     * @param companyId 企業ID
+     * @return {@link PostedResponse} が設定されたResponseEntity
+     */
 	public Optional<PostedCompany> findOne(long userId, long companyId) {
 		var response = postedCompanyDao.findByUserIdAndCompanyId(userId, companyId);
 		return Optional.ofNullable(response);
